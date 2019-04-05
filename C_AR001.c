0645f1c953130ff57ebaa4851c1927deb1fc66b9
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-04-05 23:04:09 +0800

    Add files via upload

---
+++ C_AR001.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n;
+	while(scanf("%d",&n)!=EOF)
+	{
+		int i,ary[n];
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&ary[i]);
+		}
+		for(i=n-1;i>=0;i--)
+		{
+			printf("%d ",ary[i]);
+		}
+	}
+	return 0;
+} 
