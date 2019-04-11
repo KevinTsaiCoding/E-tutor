e676570110c27adcb75b4cff608018192bff85ee
KevinTsaiCoding <48882710+KevinTsaiCoding@users.noreply.github.com>
2019-04-11 10:39:58 +0800

    Update [C_AR108-中] 英文字母Histogram.c

--- 中文題目/Array 101 - 150/[C_AR108-中] 英文字母Histogram.c
+++ 中文題目/Array 101 - 150/[C_AR108-中] 英文字母Histogram.c
@@ -1,26 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#define N 1000
+void Histogram(char *strPtr);
 int main()
 {
-	char str[100];
+	char str[N];
 	while(gets(str)!=NULL)
 	{
-		int alpha[26]={0};
-		int i;
-		for(i=0;str[i]!='\0';i++)
-		{
-			if(islower(str[i]))
-				alpha[str[i]-'a']++;
-			if(isupper(str[i]))
-				alpha[str[i]-'A']++;
-		}
-		for(i=0;i<26;i++)
-		{
-			if(alpha[i]>0)
-				printf("%c %d\n",i+97,alpha[i]);
-		}
+		Histogram(str);
 	}
 	system("pause");
 	return 0;
+}
+void Histogram(char *strPtr)
+{
+	int alpha[128]={0};
+	int i;
+	for(i=0;strPtr[i]!='\0';i++)
+	{
+		if(isupper(strPtr[i]))
+			strPtr[i]=tolower(strPtr[i]);
+		if(islower(strPtr[i]))
+			strPtr[i]=strPtr[i];
+		alpha[strPtr[i]]++;
+	}
+		
+	for(i=0;i<128;i++)
+	{
+		if(alpha[i]>0)
+			printf("%c %d\n",i,alpha[i]);
+	}
 }
